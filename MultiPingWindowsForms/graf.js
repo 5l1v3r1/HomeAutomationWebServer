28887cbc87733450180c49ab1c55bd5e7739c752
Amund Borsand <amundster@gmail.com>
2018-11-05 19:10:04 +0100

    Now keeps order of items, and partial graphs of removed items

--- MultiPingWindowsForms/graf.js
+++ MultiPingWindowsForms/graf.js
@@ -12,10 +12,10 @@ thingspeakURL = "http://2.79-160-77.customer.lyse.net:8080/last";
 
 
 function startChart() {
-  var data = new google.visualization.DataTable();
-  data.addColumn('datetime', 'Time');
-  /*data.addColumn('number', 'Temperature');
-  data.addColumn('number', 'Humidity');*/
+    var data = new google.visualization.DataTable();
+    data.addColumn('datetime', 'Time');
+    /*data.addColumn('number', 'Temperature');
+    data.addColumn('number', 'Humidity');*/
 
     var options = {
         series: {
@@ -55,57 +55,72 @@ function startChart() {
         chartArea: { left: '5%', top: '5%', width: '80%', height: '90%' }
     };
 
-  // Denne oppretter et 'google LineChart'
-  var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+    // Denne oppretter et 'google LineChart'
+    var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
 
     chart.draw(data, options);
 
     // Denne oppretter en ny funksjon, som skal gÃ¥ hver 5 sek og hente inn 1 rad med data
     (function updatethingspeakgauges() {
 
-      $.getJSON(thingspeakURL, function(json) {
+        $.getJSON(thingspeakURL, function (json) {
 
-        // Set the variables from the results array
-          if (data.getNumberOfColumns() > Object.entries(json).length + 1) { // +1 cause col[0]=timestamp
-              data.removeColumn(data.getNumberOfColumns() - 1);
-              Object.entries(json).forEach(([key, value]) => {
-                  value = null;
-              });
-          }
+            // Set the variables from the results array
+            if (data.getNumberOfColumns() > Object.entries(json).length + 1) { // +1 cause col[0]=timestamp
+                data.removeColumn(data.getNumberOfColumns() - 1);
+                Object.entries(json).forEach(([key, value]) => {
+                    value = null;
+                });
+            }
 
-		var i=0;
-        Object.entries(json).forEach(([key, value]) => {
-    			// do something with key and val
-          if (data.wg.length<=i+1)  {
-			  /*if (key.includes("%"))
-				  options.series.push({targetAxisIndex: i});*/
-			  data.addColumn('number', value + " " + key, key);
-		  }
-           else
-           	data.setColumnLabel(i+1, value + " " + key)
-          i++;				
-		});
+            var i = 0;
 
-		if (i>data.getNumberOfColumns())
-			removeColumn(i-1);
+            Object.entries(json).forEach(([key, value]) => {
+                // do something with key and val
+                if (data.wg.length <= i + 1) {
+                    /*if (key.includes("%"))
+                        options.series.push({targetAxisIndex: i});*/
+                    data.addColumn('number', value + " " + key, key);
+                }
+                /*else
+                    data.setColumnLabel(i + 1, value + " " + key)*/
+                i++;
+            });
 
-	    var myVal = [];
-        myVal=Object.values(json);
-        myVal.unshift(new Date(Date.now()));
-        console.log(myVal);
-        data.addRow(myVal);
-      });
+            /*if (i > data.getNumberOfColumns())
+                removeColumn(i - 1);*/
 
-	// Fjern første rad om den er eldre enn en time
-      var now = new Date(Date.now());
-      now.setHours(now.getHours()-6);
-      while (data.getNumberOfRows()>0 &&
-			 now > data.getValue(0,0))
-		data.removeRow(0);
+            var keys = [];
+            keys = Object.keys(json);
+            var values = [];
+            values = Object.values(json);
+            //myVal.unshift(new Date(Date.now()));
+            data.addRow();
+            var id;
+            var k;
+            for (var j = 0; j < keys.length; j++)
+                for (var i = 0; i < data.getNumberOfColumns(); i++) {
+                    if (data.getColumnId(i) == keys[j]) {
+                        data.setCell(data.getNumberOfRows() - 1, i, values[j]);
+                        data.setColumnLabel(i, values[j] + " " + keys[j]);
+                    }
+                }
 
-      chart.draw(data, options);
+            data.setCell(data.getNumberOfRows() - 1, 0, new Date(Date.now()));
+            //console.log(myVal);
+            //data.addRow(myVal);
+        });
 
-      // Denne setter timeout pÃ¥ denne funksjonen, dvs gjentar den etter 5 sek
-      setTimeout(updatethingspeakgauges, 5000);
+        // Fjern første rad om den er eldre enn en time
+        var now = new Date(Date.now());
+        now.setHours(now.getHours() - 6);
+        while (data.getNumberOfRows() > 0 &&
+            now > data.getValue(0, 0))
+            data.removeRow(0);
+
+        chart.draw(data, options);
+
+        // Denne setter timeout pÃ¥ denne funksjonen, dvs gjentar den etter 5 sek
+        setTimeout(updatethingspeakgauges, 5000);
     })();
 }
